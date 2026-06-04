dfb2ad3be2d1642201794abbf4b791ccaee00504
Miki85cz <mkuzel2010@gmail.com>
2026-06-04 20:54:30 +0200

    Beta-Testing-1.5.0

---
+++ src/main/java/com/miki/bedrockitetools/init/BedrockiteToolsModItems.java
@@ -0,0 +1,29 @@
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package com.miki.bedrockitetools.init;
+
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.DeferredItem;
+
+import net.minecraft.world.item.Item;
+
+import com.miki.bedrockitetools.item.BedrockiteSwordItem;
+import com.miki.bedrockitetools.item.BedrockiteShovelItem;
+import com.miki.bedrockitetools.item.BedrockiteHoeItem;
+import com.miki.bedrockitetools.item.BedrockiteAxeItem;
+import com.miki.bedrockitetools.BedrockiteToolsMod;
+
+public class BedrockiteToolsModItems {
+	public static final DeferredRegister.Items REGISTRY = DeferredRegister.createItems(BedrockiteToolsMod.MODID);
+	public static final DeferredItem<Item> BEDROCKITE_SWORD;
+	public static final DeferredItem<Item> BEDROCKITE_AXE;
+	public static final DeferredItem<Item> BEDROCKITE_SHOVEL;
+	public static final DeferredItem<Item> BEDROCKITE_HOE;
+	static {
+		BEDROCKITE_SWORD = REGISTRY.register("bedrockite_sword", BedrockiteSwordItem::new);
+		BEDROCKITE_AXE = REGISTRY.register("bedrockite_axe", BedrockiteAxeItem::new);
+		BEDROCKITE_SHOVEL = REGISTRY.register("bedrockite_shovel", BedrockiteShovelItem::new);
+		BEDROCKITE_HOE = REGISTRY.register("bedrockite_hoe", BedrockiteHoeItem::new);
+	}
+}
